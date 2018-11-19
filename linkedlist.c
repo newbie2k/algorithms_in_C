cd0e7da283338a363a1007b757bf7c15e0da946e
Bhaskar Singh <bhaskar.kernel@gmail.com>
2018-11-19 10:14:40 +0530

    Fixed add_node function

--- linkedlist.c
+++ linkedlist.c
@@ -13,12 +13,12 @@ struct node *start = NULL, *current = NULL;
 
 int main() {
 	int val,pos, inp;
-	int add_node(int, int);
-        int delete_node(int);
+	void add_node(int, int);
+        void delete_node(int);
 	void print_node(void);
 
 	while(1) {
-		printf("Enter Choice[1-(Add) 2-(Del) 3-(Pnt) 4-(Quit)] : ");
+		printf("Enter Choice[1-(Add) 2-(Del) 3-(Print) 4-(Quit)] : ");
 		scanf("%d",&inp);
 
 		switch(inp) {
@@ -48,39 +48,46 @@ int main() {
 
 /* Add node to Linked List */
 
-int add_node(int val, int pos) {
+void add_node(int val, int pos) {
 	int flag=0,i;
 	struct node *new;
-	current = start;
+	current = start;					//One every call setting current to start
 
 	new = malloc(sizeof(struct node));
 
-	if(pos == 1) {
-		current = start = new;
-		current->data = val;
-		current->next = NULL;
+	if((start == NULL) && (pos > 1)) {			//Checking if provide first input is Out of bound
+		printf("Error: Position is out of bound\n");
+		free(new);
+		return;
 	}
-	else {	
-		for(i=1;i<pos;i++) {
-			/* Checking if reached Last Element */
-			if((current->next == NULL) && (i != pos - 1)) {		//Need to Check Logic
-				printf("Error: Given Position out of last element");
+	
+	if(pos == 1) {						//This might look different because here our current is after new
+		start = new;
+		new->next = current;
+		new->data = val;
+	}
+	else {							//Here we will assume current is just before new
+		for(i=2;i<pos;i++) {
+			current = current->next;
+			if(current == NULL) {			//if previous node(just) is null then how we can put element after that
+				printf("Error: Position is out of bound\n");
+				free(new);
 				flag = 1;
 				break;
 			}
-			current = current->next;
 		}
-		if(flag != 1) {
+		if(flag == 0) {
+			new->next = current->next;
 			current->next = new;
 			new->data = val;
 		}
 	}
-	
-return 0;
 }
 
+
+
 /* Delete node from Linked List */
-int delete_node(int pos) {
+void delete_node(int pos) {
 	int flag = 0,i;
 	struct node *del;
 	current = start;
@@ -106,7 +113,6 @@ int delete_node(int pos) {
 			free(del);
 		}
 	}
-	return 0;
 }
 /* Linked List Print Nodes */
 void print_node() {
@@ -119,18 +125,4 @@ void print_node() {
 		current=current->next;
 		i++;
 	}
-	return;
 }
-						
-
-
-
-
-
-
-
-
-
-
-
-
